21ee5d6c89f1ed17d53b1544751f2605e249b127
fraxken <gentilhomme.thomas@gmail.com>
2023-03-19 16:09:52 +0100

    chore: update deps & code for first release

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 // Import Internal Dependencies
 import Github from "./services/github.js";
 
-export async function fetchOrgMetadata() {
-  const githubRepository = new Github(
-    process.env.GITHUB_ORG_NAME!
-  );
+export async function fetchOrgMetadata(orgName: string) {
+  const githubRepository = new Github(orgName);
 
   const { avatar_url } = await githubRepository.information();
   const projects = await githubRepository.fetchRepositories();
---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+// Import Node.js Dependencies
+import { test } from "node:test";
+import assert from "node:assert";
+
+test("empty", () => {
+  assert.ok(true);
+});
