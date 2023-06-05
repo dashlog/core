d1bc60014c97edda3965017e6e334bacc86a26c4
KimiWaDare <abbes.alexandre@gmail.com>
2023-06-05 17:22:46 +0200

    fix: fix typo, naming and indentation of the code

--- test/getCoverageLIb.spec.ts
+++ test/getCoverageLIb.spec.ts
@@ -1,22 +1,28 @@
+// Import Node.js Dependencies
 import { describe, it } from "node:test";
-import assert from "node:assert";
+import assert from "node:assert/strict";
+
+// Import Internal Dependencies
 import { getCoverageLib } from "../src/utils";
 
-describe("getCoverageLib.js", () => {
-  it("getCoverageLib should return N/A", () => {
-    assert.strictEqual(getCoverageLib({ test: "2.0.0" }), "N/A");
+describe("getCoverageLib()", () => {
+  it("Should return N/A", () => {
+    assert.equal(getCoverageLib({ test: "2.0.0" }), "N/A");
   });
 
-  it("getCoverageLib should return nyc", () => {
-    assert.strictEqual(getCoverageLib({ nyc: "2.0.0" }), "nyc");
+  it("Should return nyc", () => {
+    assert.equal(getCoverageLib({ nyc: "2.0.0" }), "nyc");
   });
-  it("getCoverageLib should return c8", () => {
-    assert.strictEqual(getCoverageLib({ c8: "2.0.0" }), "c8");
+
+  it("Should return c8", () => {
+    assert.equal(getCoverageLib({ c8: "2.0.0" }), "c8");
   });
-  it("getCoverageLib should return jest", () => {
-    assert.strictEqual(getCoverageLib({ jest: "2.0.0" }), "jest");
+
+  it("Should return jest", () => {
+    assert.equal(getCoverageLib({ jest: "2.0.0" }), "jest");
   });
-  it("getCoverageLib should return all libs", () => {
-    assert.strictEqual(getCoverageLib({ nyc: "2.0.0", c8: "1.0.5", jest: "1.6.0" }), "nyc,c8,jest");
+
+  it("Should return all libs", () => {
+    assert.equal(getCoverageLib({ nyc: "2.0.0", c8: "1.0.5", jest: "1.6.0" }), "nyc,c8,jest");
   });
 });
--- test/getTestFrameworkName.spec.ts
+++ test/getTestFrameworkName.spec.ts
@@ -1,24 +1,32 @@
+// Import Node.js Dependencies
 import { describe, it } from "node:test";
-import assert from "node:assert";
+import assert from "node:assert/strict";
+
+// Import Internal Dependencies
 import { getTestFrameworkName } from "../src/utils";
 
-describe("getTestFrameworkName.js", () => {
-  it("getTestFrameworkName should return N/A", () => {
-    assert.strictEqual(getTestFrameworkName({ test: "2.0.0", test2: "3.0.0" }), "N/A");
+describe("getTestFrameworkName()", () => {
+  it("Should return N/A", () => {
+    assert.equal(getTestFrameworkName({ test: "2.0.0", test2: "3.0.0" }), "N/A");
   });
-  it("getTestFrameworkName should return ava", () => {
-    assert.strictEqual(getTestFrameworkName({ ava: "2.0.0" }), "ava");
+
+  it("Should return ava", () => {
+    assert.equal(getTestFrameworkName({ ava: "2.0.0" }), "ava");
   });
-  it("getTestFrameworkName should return jest", () => {
-    assert.strictEqual(getTestFrameworkName({ jest: "2.0.0" }), "jest");
+
+  it("Should return jest", () => {
+    assert.equal(getTestFrameworkName({ jest: "2.0.0" }), "jest");
   });
-  it("getTestFrameworkName should return japa", () => {
-    assert.strictEqual(getTestFrameworkName({ japa: "2.0.0" }), "japa");
+
+  it("Should return japa", () => {
+    assert.equal(getTestFrameworkName({ japa: "2.0.0" }), "japa");
   });
-  it("getTestFrameworkName should return tape", () => {
-    assert.strictEqual(getTestFrameworkName({ tape: "2.0.0" }), "tape");
+
+  it("Should return tape", () => {
+    assert.equal(getTestFrameworkName({ tape: "2.0.0" }), "tape");
   });
-  it("getTestFrameworkName should return mocha", () => {
-    assert.strictEqual(getTestFrameworkName({ mocha: "2.0.0" }), "mocha");
+
+  it("Should return mocha", () => {
+    assert.equal(getTestFrameworkName({ mocha: "2.0.0" }), "mocha");
   });
 });
