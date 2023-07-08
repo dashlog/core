33607a6b0ba591e82d76b1c565ea448431932a0c
yurifa <34767221+abbesAlexandre@users.noreply.github.com>
2023-07-08 15:54:26 +0200

    test(plugins): add missing tests (#56)

---
+++ test/plugins/nodesecure.spec.ts
@@ -0,0 +1,44 @@
+// Import Node.js Dependencies
+import { after, describe, it } from "node:test";
+import assert from "node:assert";
+
+// Import Third-party Dependencies
+import { MockAgent, setGlobalDispatcher } from "undici";
+
+// Import Internal Dependencies
+import { execute } from "../../src/plugins/nodesecure";
+
+const kApiUrl = "https://api.securityscorecards.dev";
+
+describe("execute()", async() => {
+  const mockAgent = new MockAgent();
+  mockAgent.disableNetConnect();
+
+  const mockPool = mockAgent.get(kApiUrl);
+
+  setGlobalDispatcher(mockAgent);
+
+  after(async() => {
+    await mockAgent.close();
+  });
+
+  it("should not add plugin", async() => {
+    mockPool.intercept({ path: "/projects/github.com/NodeSecure/ossf-scorecard-sdk" }).reply(404).times(1);
+
+    const repo: any = { name: "ossf-scorecard-sdk", plugins: {}, package_name: null };
+
+    await execute("NodeSecure", repo);
+    assert.equal(repo.package_name, null);
+  });
+
+  it("should add plugin", async() => {
+    mockPool.intercept({ path: "/projects/github.com/NodeSecure/ossf-scorecard-sdk" }).reply(200, { foo: "bar" }, {
+      headers: { "Content-Type": "application/json" }
+    }).times(1);
+
+    const repo: any = { name: "cli", plugins: {}, package_name: "@nodesecure/cli" };
+
+    await execute("NodeSecure", repo);
+    assert.equal(repo.plugins.nodesecure.foo, "bar");
+  });
+});
