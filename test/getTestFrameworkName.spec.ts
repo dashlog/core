c553307764abeef52ea958ff500712bc86a39f2f
Kouadio Fabrice N'guessan <72697416+fabnguess@users.noreply.github.com>
2025-01-16 21:23:28 +0000

    refactor: udpate test runner detection (#96)

--- test/getTestFrameworkName.spec.ts
+++ test/getTestFrameworkName.spec.ts
@@ -7,11 +7,13 @@ import { getTestFrameworkName } from "../src/utils";
 
 describe("getTestFrameworkName()", () => {
   it("Should return N/A", () => {
-    assert.equal(getTestFrameworkName({ test: "2.0.0", test2: "3.0.0" }), "N/A");
+    assert.equal(getTestFrameworkName({ name: "test", version: "1.0.0", test: "2.0.0", test2: "3.0.0" }), "N/A");
   });
 
   it("Should return ava", () => {
     const packageJson = {
+      name: "test",
+      version: "1.0.0",
       devDependencies: {
         ava: "^2.0.0"
       }
@@ -21,6 +23,8 @@ describe("getTestFrameworkName()", () => {
 
   it("Should return jest", () => {
     const packageJson = {
+      name: "test",
+      version: "1.0.0",
       devDependencies: {
         jest: "^2.0.0"
       }
@@ -30,8 +34,10 @@ describe("getTestFrameworkName()", () => {
 
   it("Should return japa", () => {
     const packageJson = {
+      name: "test",
+      version: "1.0.0",
       devDependencies: {
-        japa: "^2.0.0"
+        "@japa/runner": "^2.0.0"
       }
     };
     assert.equal(getTestFrameworkName(packageJson), "japa");
@@ -39,6 +45,8 @@ describe("getTestFrameworkName()", () => {
 
   it("Should return tape", () => {
     const packageJson = {
+      name: "test",
+      version: "1.0.0",
       devDependencies: {
         tape: "^2.0.0"
       }
@@ -48,6 +56,8 @@ describe("getTestFrameworkName()", () => {
 
   it("Should return mocha", () => {
     const packageJson = {
+      name: "test",
+      version: "1.0.0",
       devDependencies: {
         mocha: "^2.0.0"
       }
@@ -56,12 +66,52 @@ describe("getTestFrameworkName()", () => {
     assert.equal(getTestFrameworkName(packageJson), "mocha");
   });
 
-  it("Should return node:test", () => {
-    const packageJson = {
-      scripts: {
-        test: "node --test"
-      }
-    };
-    assert.equal(getTestFrameworkName(packageJson), "node:test");
+  describe("node:test", () => {
+    it("Should return node:test for test script", () => {
+      const packageJson = {
+        name: "test",
+        version: "1.0.0",
+        scripts: {
+          test: "node --test"
+        }
+      };
+      assert.equal(getTestFrameworkName(packageJson), "node:test");
+    });
+
+    it("Should return node:test when a nested script uses node --test", () => {
+      const packageJson = {
+        name: "test",
+        version: "1.0.0",
+        scripts: {
+          test: "c8 --all --src ./src -r html npm run test-only",
+          "test-only": "glob -c \"node --loader=esmock --no-warnings --test-concurrency 1 --test\" \"test/**/*.test.js\""
+        }
+      };
+      assert.equal(getTestFrameworkName(packageJson), "node:test");
+    });
+
+    it("Should return node:test when a nested script uses node --test, recursively", () => {
+      const packageJson = {
+        name: "test",
+        version: "1.0.0",
+        scripts: {
+          test: "npm run foo",
+          foo: "npm run bar",
+          bar: "node --test"
+        }
+      };
+      assert.equal(getTestFrameworkName(packageJson), "node:test");
+    });
+
+    it("Should return node:test for test script when using tsx", () => {
+      const packageJson = {
+        name: "test",
+        version: "1.0.0",
+        scripts: {
+          test: "tsx --test"
+        }
+      };
+      assert.equal(getTestFrameworkName(packageJson), "node:test");
+    });
   });
 });
